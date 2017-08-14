29518addc1bdd2a7e8e61f57f78c596f8fab1d1f
Ryan Taus <taus.ryan@gmail.com>
2017-08-13 22:39:57 -0700

    added user functionality

--- main.py
+++ main.py
@@ -1,25 +1,10 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 import webapp2
 import jinja2
 from post import Post
 from user import User
 from google.appengine.api import users
 import time
+from datetime import datetime
 
 env = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'))
 
@@ -31,6 +16,17 @@ class MainHandler(webapp2.RequestHandler):
         if user:
             greeting = ('Welcome, %s! (<a href="%s">sign out</a>)' %
                 (user.nickname(), login_url))
+            # create user Model
+            u = users.get_current_user().nickname()
+            u = User.query(User.name == u).get()
+            if u:
+                pass
+            else:
+
+                u = User(
+                    email= users.get_current_user().email()
+                )
+                u.put()
         else:
             greeting = ('<a href="%s">Sign in or register</a>.' % login_url)
 
@@ -45,7 +41,13 @@ class MainHandler(webapp2.RequestHandler):
 class HomeHandler(webapp2.RequestHandler):
     def get(self):
         template = env.get_template('home.html')
-        self.response.write(template.render())
+
+        user = users.get_current_user().nickname()
+        user = User.query(User.name == user).get()
+        vars = {
+            'data': user
+        }
+        self.response.write(template.render(vars))
 
 
 class PostHandler(webapp2.RequestHandler):
@@ -75,8 +77,9 @@ class BrowseHandler(webapp2.RequestHandler):
     def post(self):
         post = Post(
             content=self.request.get('content'),
-            time= time.time(),
-            words_punned= self.request.get('keywords').split(',')
+            time= datetime.now(),
+            words_punned= self.request.get('keywords').split(','),
+            poster_name= users.get_current_user().nickname()
         )
         if post.content != '':
             post.put()
